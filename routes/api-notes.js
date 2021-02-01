59b061af6697df98d9bc35f86a5857484b388988
Aika Darling <zh.aikerim@gmail.com>
2021-02-01 01:09:54 -0600

    adding api routes

--- routes/api-notes.js
+++ routes/api-notes.js
@@ -1,7 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("/db.json", "utf8"));
+
 
 module.exports = function(app) {
-    app.get("/api/notes", function(req,res){
-        res.json(data)
-    })
-}
+
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
+
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
+     
+    app.post('/api/notes', (req, res ) => {
+    let newNotes = req.body;
+    let newData = (data.length).toString()
+console.log(newData)
+    newNotes.push(newTable)
+    res.json(newTable)
+    fs.writeFileSync("/db.json", JSON.stringify(data), function(err) {
+                if (err) throw (err);        
+            }); 
+    
+            res.json(data);    
+    
+});
--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+
+
 app.use(express.static('public'))
+
+app.use("/assets", express.static("./assets"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-})
-
-app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
+
+require("./routes/api-notes")(app);
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
