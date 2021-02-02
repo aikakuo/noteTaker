f65e8854782c84e20eccfe076fd577ef986aae7a
Aika Darling <zh.aikerim@gmail.com>
2021-02-02 00:49:11 -0600

    updated readme

--- routes/api-notes.js
+++ routes/api-notes.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 let {notes,highest} = JSON.parse(fs.readFileSync("./db.json", "utf8"));
 
-
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
@@ -30,11 +29,33 @@ module.exports = function(app) {
     
             res.json(notes);    
     })
+    //read all notes from the `db.json`
+    app.put("/api/notes/:id", (req,res) => {
+        fs.readFile("./db.json",JSON.stringify({highest,notes}), function (err) {
+            if (err) throw (err);
+            } )
+    });
     
     app.delete("/api/notes/:id", function(req, res) {
-        console.log(req.params)
+        const noteId = (req.params.id)
+        console.log(`Deleting ID : ${noteId}`)
         // res.json(data[Number(req.params.id)]);
         //put route 
         //put /patch diff
     });
+    //remove the note with the given `id` property
+        // app.delete("/api/notes/highest", (req, res) => {
+        //     let notesData = [];
+          
+        //     const noteId = req.params.id
+        //     console.log(`Deleting ${noteId}`)
+        //     fs.writeFile("./db.json",JSON.parse({notesData}),
+        //     function (err) {
+        //         if (err) throw (err);
+        //     } )
+          
+        // });
 };
+//rewrite the notes to the `db.json` file.
+
+//}
