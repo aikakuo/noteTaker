8624ab23092d77185eb6ec4dafed974bbbfbfbf1
Aika Darling <zh.aikerim@gmail.com>
2021-02-01 21:10:21 -0600

    notes adding to db.json with defined id's

--- routes/api-notes.js
+++ routes/api-notes.js
@@ -1,31 +1,40 @@
 const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("/db.json", "utf8"));
+let {notes,highest} = JSON.parse(fs.readFileSync("./db.json", "utf8"));
 
 
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-       
-        res.json(data);
+        
+        res.json(notes);
 
     });
 
     app.get("/api/notes/:id", function(req, res) {
 
-        res.json(data[Number(req.params.id)]);
+        res.json(notes[Number(req.params.id)]);
 
     });
      
     app.post('/api/notes', (req, res ) => {
     let newNotes = req.body;
-    let newData = (data.length).toString()
-console.log(newData)
-    newNotes.push(newTable)
-    res.json(newTable)
-    fs.writeFileSync("/db.json", JSON.stringify(data), function(err) {
+    //let newData = (notes.length).toString()
+    let notesID = highest = highest + 1
+    newNotes.id = notesID
+    console.log(newNotes)
+    notes.push(newNotes)
+    // res.json(newNotes)
+    fs.writeFile("./db.json", JSON.stringify({highest,notes}), function(err) {
                 if (err) throw (err);        
             }); 
     
-            res.json(data);    
+            res.json(notes);    
+    })
     
-});
+    app.delete("/api/notes/:id", function(req, res) {
+        console.log(req.params)
+        // res.json(data[Number(req.params.id)]);
+        //put route 
+        //put /patch diff
+    });
+};
